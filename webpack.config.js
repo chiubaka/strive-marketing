087fd0d3044f01558c3b850e0450ed20c3b8c771
Daniel Chiu <daniel@chiubaka.com>
2017-09-28 18:57:29 -0700

    Minimize JS in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const config = require("./webpack.config.dev.js");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Uglify = require("uglifyjs-webpack-plugin");
 
 config.plugins.push(new ExtractTextPlugin({
   filename: "strive.css",
   allChunks: true
 }));
+
+config.plugins.push(new Uglify());
 
 module.exports = config;
