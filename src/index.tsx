4fd209f545f9e5a73ad471d450eea64510a48aab
Daniel Chiu <daniel@chiubaka.com>
2017-10-02 22:33:26 -0700

    Add Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
 import "bootstrap";
+import * as analytics from "universal-ga";
 
 import "../styles/index.scss";
 
 import Home from "./pages/Home";
+
+analytics.initialize("UA-58023025-2");
 
 render(
   <Home/>,
