c4d700e347413ca78d6d2d2f97b4fa2c25447f06
Daniel Chiu <daniel@chiubaka.com>
2017-10-03 12:01:59 -0700

    Re-configure Google Analytics since it wasn't working

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import * as React from "react";
 import { render } from "react-dom";
 import "bootstrap";
-import * as analytics from "universal-ga";
 
 import "../styles/index.scss";
 
 import Home from "./pages/Home";
-
-analytics.initialize("UA-58023025-2");
 
 render(
   <Home/>,
