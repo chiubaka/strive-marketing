8491a68ee1813a8f03c34e58866f5ff978dbbdff
Daniel Chiu <daniel@chiubaka.com>
2017-10-03 17:24:51 -0700

    Minor style changes and re-order features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -14,38 +14,6 @@ export default class Features extends React.Component {
             <ul>
               <li>
                 <div className="icon-container">
-                  <span className="oi oi-phone" title="phone" aria-hidden="true"></span>
-                </div>
-                <p className="font-weight-bold">Mobile support.</p>
-                <p>Take Strive with you on your mobile device wherever you go.</p>
-              </li>
-              <li>
-                <div className="icon-container">
-                  <span className="oi oi-lightbulb" title="lightbulb" aria-hidden="true"></span>
-                </div>
-                <p className="font-weight-bold">Insight generation.</p>
-                <p>Learn how you can improve with Strive's automatic analysis of your progress.</p>
-              </li>
-              <li>
-                <div className="icon-container">
-                  <span className="oi oi-task" title="task" aria-hidden="true"></span>
-                </div>
-                <p className="font-weight-bold">Habit tracking.</p>
-                <p>Build and track the habits you need to succeed.</p>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <div className="icon-container">
-                  <span className="oi oi-compass" title="compass" aria-hidden="true"></span>
-                </div>
-                <p className="font-weight-bold">Intelligent reminders.</p>
-                <p>Ensure you're always in the right place at the right time with location-, time-, and context-based reminders.</p>
-              </li>
-              <li>
-                <div className="icon-container">
                   <span className="oi oi-list" title="list" aria-hidden="true"></span>
                 </div>
                 <p className="font-weight-bold">Sample plans for common goals.</p>
@@ -58,8 +26,41 @@ export default class Features extends React.Component {
                 <p className="font-weight-bold">Built-in accountability.</p>
                 <p>Share your goals and your progress with the world, or with just your close friends.</p>
               </li>
+              <li>
+                <div className="icon-container">
+                  <span className="oi oi-compass" title="compass" aria-hidden="true"></span>
+                </div>
+                <p className="font-weight-bold">Intelligent reminders.</p>
+                <p>Ensure you're always in the right place at the right time with location-, time-, and context-based reminders.</p>
+              </li>
             </ul>
           </div>
+          <div>
+            <ul>
+              <li>
+                <div className="icon-container">
+                  <span className="oi oi-task" title="task" aria-hidden="true"></span>
+                </div>
+                <p className="font-weight-bold">Habit tracking.</p>
+                <p>Build and track the habits you need to succeed.</p>
+              </li>
+              <li>
+                <div className="icon-container">
+                  <span className="oi oi-lightbulb" title="lightbulb" aria-hidden="true"></span>
+                </div>
+                <p className="font-weight-bold">Insight generation.</p>
+                <p>Learn how you can improve with Strive's automatic analysis of your progress.</p>
+              </li>
+              <li>
+                <div className="icon-container">
+                  <span className="oi oi-phone" title="phone" aria-hidden="true"></span>
+                </div>
+                <p className="font-weight-bold">Mobile support.</p>
+                <p>Take Strive with you on your mobile device wherever you go.</p>
+              </li>
+            </ul>
+          </div>
+
         </div>
         <CallToAction text="Take action"/>
       </section>
