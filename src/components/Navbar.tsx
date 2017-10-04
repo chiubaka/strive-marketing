70d9020e3fcc07db11ca03354eb2fb5072d2366b
Daniel Chiu <daniel@chiubaka.com>
2017-10-03 21:34:15 -0700

    Add Strive logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 
+const logo = require("../../resources/img/strive_logo.png");
+
 export default class Navbar extends React.Component {
   public render(): JSX.Element {
     return (
       <div className="container pt-3">
         <nav className="navbar navbar-expand-sm navbar-light">
-          <a className="navbar-brand" href="#">Strive</a>
+          <a className="navbar-brand" href="#">
+            <img src={logo} width="30" height="30" alt="Strive" className="d-inline-block align-top"/>
+            Strive
+          </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
+            <ul className="navbar-nav ml-auto align-top">
               <li className="nav-item active">
                 <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
               </li>
